19e96cac826a6daca725a76e566bf761a6fc3244
Tran Thang <tranthang.am1@gmail.com>
2026-02-18 05:38:58 +0200

    cors change

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const folderRoutes = require('./src/routes/folder_routes');
 const trashRoutes = require('./src/routes/trash_routes');
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], allowedHeaders: ["Content-Type", "Authorization"], exposedHeaders: ["Authorization"], credentials: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => { console.log(`${req.method} ${req.url}`); next();});
